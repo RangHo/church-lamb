94804318a6027424165133ee35de93a12f82f3d1
RangHo Lee <hello@rangho.me>
2023-04-12 19:24:53 +0900

    [wip] Create the skeleton for the REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    // Read, evaluate, print, and loop
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        // For now, spit whatever was input back
+        println!("{}", input);
+    }
 }
